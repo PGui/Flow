ac9f63830f6b90cd2373ccf5e3a6a7cd65b046e9
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2019-11-24 17:18:29 +0100

    examples
    Former-commit-id: fffa67923d95159c59828fce39e18bcc344af27a

---
+++ Source/FlowExamples/FirstPerson/FlowExamplesGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlowExamplesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFlowExamplesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFlowExamplesGameMode();
+};
---
+++ Source/FlowExamples/FlowExamples.cpp
@@ -0,0 +1,5 @@
+#include "FlowExamples.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlowExamples, "FlowExamples" );
+ 
