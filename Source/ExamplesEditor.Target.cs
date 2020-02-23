95c4028dfd0fee6b3d02e6707fbb45c4fa26e895
Doctor Ergot <krzysiek.justynski@gmail.com>
2020-02-23 03:58:37 +0100

    debug tools

--- Source/ExamplesEditor.Target.cs
+++ Source/ExamplesEditor.Target.cs
@@ -1,12 +1,12 @@
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class ExamplesEditorTarget : TargetRules
 {
 	public ExamplesEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		//DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.Add("Examples");
-	}
+
+        //DefaultBuildSettings = BuildSettingsVersion.V2;
+    }
 }
