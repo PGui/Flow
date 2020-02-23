2df43cccc0a76134c1c8ddbd95b391c5ef074727
Doctor Ergot <krzysiek.justynski@gmail.com>
2020-02-23 03:58:37 +0100

    debug tools
    Former-commit-id: 95c4028dfd0fee6b3d02e6707fbb45c4fa26e895

--- Source/Examples.Target.cs
+++ Source/Examples.Target.cs
@@ -1,12 +1,12 @@
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class ExamplesTarget : TargetRules
 {
 	public ExamplesTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		//DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.Add("Examples");
-	}
+
+        //DefaultBuildSettings = BuildSettingsVersion.V2;
+    }
 }
--- Source/Examples/Examples.Build.cs
+++ Source/Examples/Examples.Build.cs
@@ -2,10 +2,29 @@ using UnrealBuildTool;
 
 public class Examples : ModuleRules
 {
-	public Examples(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public Examples(ReadOnlyTargetRules Target) : base(Target)
+    {
+        bUseUnity = false;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+                "HeadMountedDisplay"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Slate",
+                "SlateCore",
+                "UMG"
+            }
+        );
+    }
 }
